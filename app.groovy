6a670b760d4626cfba80c2646ce99e58969f6c48
tykim89 <jusaha1109@gmail.com>
2017-11-26 16:35:46 +0900

    html을 반환하는 app.groovy 수정

--- app.groovy
+++ app.groovy
@@ -3,6 +3,10 @@ class App {
 	
 	@RequestMapping("/")
 	def home(){		//리퀘스트 핸들러
-		"Hello!"
+		def header = "<html><body>"
+		def footer = "</body></html>"
+		def content = "<h1>Hello!</h1><p>this is html content.</p>"
+
+		header + content + footer
 	}
 }
