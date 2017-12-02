2db0bd9c4e513cbd826176c9a38dcef2d5e29baa
tykim89 <jusaha1109@gmail.com>
2017-12-02 17:31:44 +0900

    객체를 Json으로 리턴

--- MyBootApp/src/main/java/com/ty/springboot/HelloController.java
+++ MyBootApp/src/main/java/com/ty/springboot/HelloController.java
@@ -7,12 +7,48 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-	@RequestMapping("/{num}")
-	public String index(@PathVariable int num){
-		int res = 0;
-		for(int i=1; i<=num; i++){
-			res += i;
-		}
-		return "total : " + res;
+	String[] names = {"kim", "lee", "park", "choi", "jo"};
+	String[] mails = {"kim@taeyoung.com", "lee@flower", "park@yamada", "choi@happy", "jo@baseball"};
+
+	@RequestMapping("/{id}")
+	public DataObject index(@PathVariable int id){
+		return new DataObject(id, names[id], mails[id]);
+	}
+}
+
+
+class DataObject {
+	private int id;
+	private String name;
+	private String value;
+	
+	public DataObject(int id, String name, String value){
+		this.id = id;
+		this.name = name;
+		this.value = value;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
 	}
 }
