83a4ca3ae0264b941199b71da9137d37118f45ac
tykim89 <jusaha1109@gmail.com>
2017-12-15 20:29:41 +0900

    ${object} 속성과 *{ } 프로퍼티 참조

--- MyBootApp/src/main/java/com/ty/springboot/HelloController.java
+++ MyBootApp/src/main/java/com/ty/springboot/HelloController.java
@@ -12,17 +12,47 @@ public class HelloController {
 	@RequestMapping(value="/")
 	public ModelAndView index(ModelAndView mav){
 		mav.setViewName("index");
+		mav.addObject("msg", "current data.");
+		DataObject obj = new DataObject(123, "tykim", "ty_1109.kim@samsung.com");
+		mav.addObject("object", obj);
 		return mav;
 	}
+
 	
-	@RequestMapping("/other")
-	public ModelAndView other(){
-		return new ModelAndView("redirect:/");
+	class DataObject {
+	    private int id;
+	    private String name;
+	    private String value;
+	    
+	    public DataObject(int id, String name, String value){
+	        this.id = id;
+	        this.name = name;
+	        this.value = value;
+	    }
+
+	    public int getId() {
+	        return id;
+	    }
+
+	    public void setId(int id) {
+	        this.id = id;
+	    }
+
+	    public String getName() {
+	        return name;
+	    }
+
+	    public void setName(String name) {
+	        this.name = name;
+	    }
+
+	    public String getValue() {
+	        return value;
+	    }
+
+	    public void setValue(String value) {
+	        this.value = value;
+	    }
 	}
-	
-	@RequestMapping("/home")
-	public String home(){
-		return "forward:/";
-	}
-	
+
 }
