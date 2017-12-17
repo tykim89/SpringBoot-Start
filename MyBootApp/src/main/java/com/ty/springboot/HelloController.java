0cfef8356563672858e9779412b093285e6852e2
tykim89 <jusaha1109@gmail.com>
2017-12-17 18:02:13 +0900

    조건분기 th:if 추가

--- MyBootApp/src/main/java/com/ty/springboot/HelloController.java
+++ MyBootApp/src/main/java/com/ty/springboot/HelloController.java
@@ -14,9 +14,9 @@ public class HelloController {
 	public ModelAndView index(@PathVariable int id, ModelAndView mav){
 		mav.setViewName("index");
 		mav.addObject("id", id);
-		mav.addObject("check", id%2 == 0);
-		mav.addObject("trueVal", "Even number!");
-		mav.addObject("falseVal", "Odd number...");
+		mav.addObject("check", id >= 0);
+		mav.addObject("trueVal", "POSITIVE!");
+		mav.addObject("falseVal", "negative...");
 		return mav;
 	}
 
