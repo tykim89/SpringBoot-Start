59a2c7302ae35f45aa64881602c6213c09ec69d6
tykim89 <jusaha1109@gmail.com>
2017-12-17 17:56:59 +0900

    조건식 추가

--- MyBootApp/src/main/java/com/ty/springboot/HelloController.java
+++ MyBootApp/src/main/java/com/ty/springboot/HelloController.java
@@ -1,6 +1,7 @@
 package com.ty.springboot;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,48 +10,15 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class HelloController {
 
-	@RequestMapping(value="/")
-	public ModelAndView index(ModelAndView mav){
+	@RequestMapping(value="/{id}")
+	public ModelAndView index(@PathVariable int id, ModelAndView mav){
 		mav.setViewName("index");
-		mav.addObject("msg", "message 1<hr/>message 2<br/>message 3");
+		mav.addObject("id", id);
+		mav.addObject("check", id%2 == 0);
+		mav.addObject("trueVal", "Even number!");
+		mav.addObject("falseVal", "Odd number...");
 		return mav;
 	}
 
 	
-/*	class DataObject {
-	    private int id;
-	    private String name;
-	    private String value;
-	    
-	    public DataObject(int id, String name, String value){
-	        this.id = id;
-	        this.name = name;
-	        this.value = value;
-	    }
-
-	    public int getId() {
-	        return id;
-	    }
-
-	    public void setId(int id) {
-	        this.id = id;
-	    }
-
-	    public String getName() {
-	        return name;
-	    }
-
-	    public void setName(String name) {
-	        this.name = name;
-	    }
-
-	    public String getValue() {
-	        return value;
-	    }
-
-	    public void setValue(String value) {
-	        this.value = value;
-	    }
-	}
-*/
 }
