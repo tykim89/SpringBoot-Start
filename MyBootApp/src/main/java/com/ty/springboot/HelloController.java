e0d67c351c0d6cc1005fce1374ce5fbecaeb9864
tykim89 <jusaha1109@gmail.com>
2017-12-17 18:51:05 +0900

    뷰에서의 전처리

--- MyBootApp/src/main/java/com/ty/springboot/HelloController.java
+++ MyBootApp/src/main/java/com/ty/springboot/HelloController.java
@@ -3,22 +3,66 @@ package com.ty.springboot;
 import java.util.ArrayList;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController {
 
-	@RequestMapping(value="/")
-	public ModelAndView index(ModelAndView mav){
+	@RequestMapping(value="/{num}")
+	public ModelAndView index(@PathVariable int num, ModelAndView mav){
 		mav.setViewName("index");
-		ArrayList<String[]> data = new ArrayList<String[]>();
-		data.add(new String[]{"park", "park@yamada", "090-999-999"});
-		data.add(new String[]{"lee", "lee@flower", "010-111-1111"});
-		data.add(new String[]{"choi", "choi@sony", "055-555-555"});
+		mav.addObject("num", num);
+		
+		if(num >= 0){
+			mav.addObject("check", "num >= data.size() ? 0 : num");
+		}else{
+			mav.addObject("check", "num <= data.size()*-1 ? 0 : num*-1");
+		}
+		
+		ArrayList<DataObject> data = new ArrayList<DataObject>();
+		data.add(new DataObject(0, "park", "park@yamada"));
+		data.add(new DataObject(1, "lee", "lee@flower"));
+		data.add(new DataObject(2, "choi", "choi@sony"));
 		mav.addObject("data", data);
 		return mav;
 	}
-
+ 
+	class DataObject {
+	    private int id;
+	    private String name;
+	    private String value;
+	    
+	    public DataObject(int id, String name, String value){
+	        this.id = id;
+	        this.name = name;
+	        this.value = value;
+	    }
 	
+	    public int getId() {
+	        return id;
+	    }
+	
+	    public void setId(int id) {
+	        this.id = id;
+	    }
+	
+	    public String getName() {
+	        return name;
+	    }
+	
+	    public void setName(String name) {
+	        this.name = name;
+	    }
+	
+	    public String getValue() {
+	        return value;
+	    }
+	
+	    public void setValue(String value) {
+	        this.value = value;
+	    }
+	}
+
 }
